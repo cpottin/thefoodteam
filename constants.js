06b4e380301a46cf64ecef9fbfffde843c03b203
sw <aaeuio@gmail.com>
2022-12-18 18:13:37 -0800

    add error.js constants.js; recipe.js imports handleError

---
+++ constants.js
@@ -0,0 +1,6 @@
+export const DEFAULT_ERROR_MESSAGE =
+  "API Call Error: Please double check your URL and make sure the server is up & running.";
+export const NO_FOUND_RECIPE = "No recipe found on the recipe API.";
+
+// export const DEFAULT_PHOTO_URL =
+//   "https://images.unsplash.com/photo-1598899134739-24c46f58b8c0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1456&q=80";
