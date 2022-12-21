46fddbb4225c2ec5f17fae4f8a40bb2761f50994
drod32 <drod32@gmail.com>
2022-12-21 15:27:25 -0500

    Changed index.html layout -added media query to weather -adjusted card and image sizing -changes to background colors, margins

--- recipes.js
+++ recipes.js
@@ -11,8 +11,8 @@ import { DEFAULT_IMAGE } from "./constants.js";
 
 // Load search result cards
 export function getRecipes(search) {
-  let apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${CINDY2_SPOON_KEY}&query=${search}&addRecipeInformation=true&instructionsRequired=true&number=6`;
-  //let apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${CINDYS_SPPON_KEY}&query=${search}&addRecipeInformation=true&instructionsRequired=true&number=6`;
+  // let apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${CINDY2_SPOON_KEY}&query=${search}&addRecipeInformation=true&instructionsRequired=true&number=6`;
+  let apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${CINDYS_SPPON_KEY}&query=${search}&addRecipeInformation=true&instructionsRequired=true&number=6`;
   //let apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${SHUOFEIS_SPOON_KEY}&query=${search}&addRecipeInformation=true&instructionsRequired=true&number=6`;
   //let apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${SHUOFEIS2_SPOON_KEY}&query=${search}&addRecipeInformation=true&instructionsRequired=true&number=6`;
   //let apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${DAVIDS_SPOON_KEY}&query=${search}&addRecipeInformation=true&instructionsRequired=true&number=6`;
@@ -26,7 +26,7 @@ export function getRecipes(search) {
 
       for (let i = 0; i < json.results.length; i++) {
         card_deck.innerHTML += `
-                <div class="card">
+                <div class="card result">
                     <img
                     class="card-img-top"
                     src="${
@@ -39,13 +39,13 @@ export function getRecipes(search) {
                 <div class="card-body">
                 <h5 class="card-title">${json.results[i].title}</h5>
                 <p class="card-text">
-                <small class="text-muted">Time to cook: ${
+                <small class="text-muted">Ready In: ${
                   json.results[i].readyInMinutes
                 } mins</small>
                 </p>
                 <button type="button" id="${
                   json.results[i].id
-                }" class="btn btn-warning recipe_card_btn">
+                }" class="btn btn-info recipe_card_btn">
                 Go to Recipe Card
                 </button>
                 </div>
@@ -68,8 +68,8 @@ export function gotoRecipe(event) {
     //get ID from the button when the id is the id from the first call
   const recipeID = recipeBtn.id;
     //get button by class with classlist
-  let recipeUrl = `https://api.spoonacular.com/recipes/${recipeID}/card?apiKey=${CINDY2_SPOON_KEY}`
-  // let recipeUrl = `https://api.spoonacular.com/recipes/${recipeID}/card?apiKey=${CINDYS_SPPON_KEY}`
+  // let recipeUrl = `https://api.spoonacular.com/recipes/${recipeID}/card?apiKey=${CINDY2_SPOON_KEY}`
+  let recipeUrl = `https://api.spoonacular.com/recipes/${recipeID}/card?apiKey=${CINDYS_SPPON_KEY}`
   // let recipeUrl = `https://api.spoonacular.com/recipes/${recipeID}/card?apiKey=${SHUOFEIS_SPOON_KEY}`
   // let recipeUrl = `https://api.spoonacular.com/recipes/${recipeID}/card?apiKey=${SHUOFEIS2_SPOON_KEY}`
   // let recipeUrl = `https://api.spoonacular.com/recipes/${recipeID}/card?apiKey=${DAVIDS_SPOON_KEY}`
